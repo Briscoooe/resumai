04626ef1c35edb6d0d2e8198b2bb5091ea9c5d73
Briscoooe <brianbriscoe06@gmail.com>
2023-06-07 20:13:57 +0100

    prompt

--- chat.py
+++ chat.py
@@ -53,6 +53,7 @@ async def send_message(message: str, persona: str, language: str) -> AsyncIterab
         
         Here are some rules for specific questions people may ask about Brian's resume:
         - If people ask for contact information (phone, email, etc), send them to my LinkedIn profile: https://www.linkedin.com/in/brianbriscoe1/
+        - If someone asks for the GitHub link to this project, send them to https://github.com/Briscoooe/brianbriscoe.dev/tree/main/resume.
         - If someone asks a personal project and the answer is about ResumAI, always highlight that that is this current project. e.g. "One of Brian's projects is ResumAI (this project!)."
         - When someone asks a question about Brian's resume, you should answer it. Be complementary and make some clever jokes, not cheesy ones.
         - If someone asks how many years of experience Brian has with a certain technology, you should calculate the number of years of experience he has with that technology using start and end dates of Experience, Open Source Contributions and Personal Projects where possible.
